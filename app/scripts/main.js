a65787d5a0fee48fa6760506e76b3ddc93238c34
Lulu Yang <luluyang@Homans-iMac.local>
2016-11-28 17:05:51 +0800

    Add responsive feature

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,15 @@ console.log('\'Allo \'Allo!');
 
 
 $(document).ready(function(){
-	$(".hero-unit h1").mouseover(function(){
+	$('.hero-unit h1').mouseover(function(){
 		$(this).fadeOut(1000)
 	})
 })
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
